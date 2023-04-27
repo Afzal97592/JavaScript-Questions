fba98b2d06882080780f05c4d746ebaa7bfc8aae
Afzal97592 <mail2afzalahmad@gmail.com>
2023-04-27 09:11:17 +0530

    Master Branch Also Changed

--- Day04.js
+++ Day04.js
@@ -160,3 +160,8 @@ function maxProfit(prices){
 }
 console.log(maxProfit([7, 1, 5, 3, 6, 4]))
 // console.log(maxProfit([7, 6, 4, 3, 1]))
+
+// checking branch merging
+// Start
+// developer
+// End
