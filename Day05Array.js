825ba32cba1e60856ab81cc034a6b44ad90eba50
Afzal97592 <mail2afzalahmad@gmail.com>
2023-04-30 09:05:56 +0530

    Array05Day3

--- Day05Array.js
+++ Day05Array.js
@@ -334,4 +334,86 @@ function removeDuplicatesNew(nums){
     }
     return i+1
 }
-console.log(removeDuplicatesNew([0,0,1,1,2,2,3,3,4]))
+// console.log(removeDuplicatesNew([0,0,1,1,2,2,3,3,4]))
+
+
+
+
+
+//  ===>>> Leet Code Question
+
+// ------>>> Question Moves Zeros to end
+// Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+// Note that you must do this in-place without making a copy of the array.
+
+// Example 1:
+
+// Input: nums = [0,1,0,3,12]
+// Output: [1,3,12,0,0]
+// Example 2:
+
+// Input: nums = [0]
+// Output: [0]
+
+
+//  Approach - 1 Brute force
+
+function moveZeroes(nums) {
+    for(let i = 0; i<nums.length; i++){
+        for(let j = i+1; j<nums.length; j++){
+            if(nums[i] === 0) {
+                 let temp = nums[i];
+                 nums[i] = nums[j];
+                 nums[j] = temp
+            }
+        }
+    }
+    return nums
+};
+
+// console.log(moveZeroes([0,1,0,3,12]))
+
+
+
+// ----->>> Leet Code Question
+
+
+// Given a 0-indexed integer array nums of size n, find the maximum difference between nums[i] and nums[j] (i.e., nums[j] - nums[i]), such that 0 <= i < j < n and nums[i] < nums[j].
+
+// Return the maximum difference. If no such i and j exists, return -1.
+
+// Example 1:
+
+// Input: nums = [7,1,5,4]
+// Output: 4
+// Explanation:
+// The maximum difference occurs with i = 1 and j = 2, nums[j] - nums[i] = 5 - 1 = 4.
+// Note that with i = 1 and j = 0, the difference nums[j] - nums[i] = 7 - 1 = 6, but i > j, so it is not valid.
+// Example 2:
+
+// Input: nums = [9,4,3,2]
+// Output: -1
+// Explanation:
+// There is no i and j such that i < j and nums[i] < nums[j].
+// Example 3:
+
+// Input: nums = [1,5,2,10]
+// Output: 9
+// Explanation:
+// The maximum difference occurs with i = 0 and j = 3, nums[j] - nums[i] = 10 - 1 = 9.
+
+function maximumDifference(nums) {
+    let maxDiff = -1;
+    for(let i = 0; i<nums.length; i++){
+        for(let j = i+1; j<nums.length; j++){
+            if(nums[i]<nums[j]){
+               if(maxDiff < nums[j]-nums[i]){
+                maxDiff = nums[j]-nums[i]
+               }
+            }
+        }
+    }
+   return maxDiff 
+};
+console.log(maximumDifference([1,5,2,10]))
