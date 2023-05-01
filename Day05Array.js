aae49d4dd766535d0c0a724ed729f939e16394a3
Afzal97592 <mail2afzalahmad@gmail.com>
2023-05-01 09:32:24 +0530

    Array05Day4

--- Day05Array.js
+++ Day05Array.js
@@ -416,4 +416,61 @@ function maximumDifference(nums) {
     }
    return maxDiff 
 };
-console.log(maximumDifference([1,5,2,10]))
+// console.log(maximumDifference([1,5,2,10]))
+
+
+
+// --->>> Leet Code Question 
+
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,3,1]
+// Output: true
+// Example 2:
+
+// Input: nums = [1,2,3,4]
+// Output: false
+// Example 3:
+
+// Input: nums = [1,1,1,3,3,4,3,2,4,2]
+// Output: true
+
+
+
+// ---->>> Brute force Approach
+
+ function containsDuplicate(nums) {
+    for(let i = 0; i<nums.length; i++){
+        for(let j = i+1; j<nums.length; j++){
+            if(nums[i]=== nums[j]){
+                return true
+            }
+        }
+    }
+    return false 
+};
+// console.log(containsDuplicate([1,2,3,1]))
+
+
+
+ function intersection(nums1, nums2) {
+
+    let result = []
+
+    for(let i = 0; i<nums1.length; i++){
+        for (let j = 0; j<nums2.length; j++){
+            if(nums1[i] === nums2[j]){
+                  result.push(nums1[i])
+            }
+        }
+    }
+    return Array.from(new Set(result))
+};
+console.log(intersection([4,9,5], [9,4,9,8,4]))
+
+
+
